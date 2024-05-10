342792dbfda4816106cd617e2db8d9dbc594be8e
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-05-10 13:09:37 +0100

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import { logo } from "@/assets";
+import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return (
+		<nav className="w-full flex md:justify-center justify-between  items-center p-4">
+			<div>
+				<Image src={logo} alt="logo"  className="w-32 cursor-pointer" />
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
