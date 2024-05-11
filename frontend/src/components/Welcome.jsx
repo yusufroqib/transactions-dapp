f08d4e6533ff1fd77eaf236fc0c205483d7cf92d
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-05-11 14:08:49 +0100

    update

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white";
+
+  // const inputStyles = "my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism";
+
+  const Input = ({ placeholder, name, type, value, handleChange }) => (
+      <input
+          placeholder={placeholder}
+          type={type}
+          step="0.0001"
+          value={value}
+          onChange={(e) => handleChange(e, name)}
+          className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+      />
+  );
+  
 const Welcome = () => {
-	const commonStyles =
-		"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white";
 
-    // const inputStyles = "my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism";
-
-    const Input = ({ placeholder, name, type, value, handleChange }) => (
-        <input
-            placeholder={placeholder}
-            type={type}
-            step="0.0001"
-            value={value}
-            onChange={(e) => handleChange(e, name)}
-            className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-        />
-    );
-    
 	return (
 		<div className="flex w-full justify-center items-center">
 			<div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -65,6 +66,24 @@ const Welcome = () => {
 					</div>
           
           <div>
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send now
+              </button>
+            )}
 
           </div>
 
