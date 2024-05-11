ed4fa90c15457c2c4cd37bc1046537bbfce916c4
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-05-11 13:26:44 +0100

    update

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import {SiEthereum} from 'react-icons/si'
-import {BsInfoCircle} from 'react-icons/bs'
-
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
 
 const Welcome = () => {
 	const commonStyles =
@@ -30,19 +29,23 @@ const Welcome = () => {
 					</div>
 				</div>
 
-    <div>
-      <div>
-        <div>
-          <div>
-            <div>
-              <SiEthereum fontSize={21} color="#fff" />
-            </div>
-            <BsInfoCircle fontSize={17} color="#fff"/>
-          </div>
-        </div>
-      </div>
-    </div>
-
+				<div>
+					<div className="flex flex-col flex-1 items-start justify-start w-full mf:mt-0 mt-10">
+						<div className="p-3 justify-end items-start flex-col rounded-xl  h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+							<div className="flex justify-between flex-col w-full h-full">
+								<div className="flex justify-between items-start">
+									<div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+										<SiEthereum fontSize={21} color="#fff" />
+									</div>
+									<BsInfoCircle fontSize={17} color="#fff" />
+								</div>
+							</div>
+              <p>
+                
+              </p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
